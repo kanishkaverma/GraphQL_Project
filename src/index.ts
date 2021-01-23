4b8320811c24caa0c67f6928ad51e375842f79d6
kanishka verma <kanishkaverma@users.noreply.github.com>
2021-01-24 03:07:53 +0530

    Initialized a graphql server using type-orm and express. ��✔✔

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/post';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import microConfig from './mikro-orm.config';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+  app.listen(4000, () => {
+    console.log('server started on localhost port 4000');
+  });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
+
+  apolloServer.applyMiddleware({ app });
+  //   const post = orm.em.create(Post, { title: 'my first post.' });
+  //   orm.em.persistAndFlush(post);
+};
+
+main();
+
+console.log('hello there');
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,15 @@
+import { __prod__ } from './constants';
+import { Post } from './entities/post';
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'), // path to folder with migration files
+    pattern: /^[\w-]+\d+\.[tj]s$/ // how to match migration files
+  },
+  entities: [Post],
+  dbName: 'graphql_project',
+  type: 'postgresql',
+  debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
